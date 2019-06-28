7de74eba135d346e0674c3c9bf2eaeea2c46194b
Irina Afinogenova <Irina_Afinogenova@epam.com>
2019-06-28 17:27:36 +0300

    add Redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './components/App';
+import reducer from './reducers/reducer';
 import './styles/app.scss';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const rootElement = document.getElementById('app');
+
+ReactDOM.render(
+    <Provider store={createStore(reducer)}>
+        <App />
+    </Provider>, rootElement);
---
+++ src/reducers/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+    visibilityFilter: [],
+    todos: []
+};
+
+const todoApp = (state) => {
+    if (typeof state === 'undefined') {
+        return initialState;
+    }
+
+
+    return state;
+};
+
+export default todoApp;
